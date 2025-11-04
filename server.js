2ea93e841700db6eba2ed8796b7be836d8e64643
payal8parkhe <150655953+payal8parkhe@users.noreply.github.com>
2025-11-04 18:37:39 +0530

    Updated  server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send(`
-    <h1>🚀 Hello From Zero Downtime Hub!</h1>
+    <h1>🚀 Hello From ZDH!</h1>
     <p>This app was deployed via API.</p>
     <p>Time: ${new Date().toLocaleString()}</p>
   `);
